3156206670f5ed451c3d043de335058306c4d5c1
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2020-01-20 11:35:43 +0100

    Move protos one folder up and add proto for interacting with the main system

--- factory_functional_units/build.rs
+++ factory_functional_units/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(false)
         .out_dir("src/server")
-        .compile(&["proto/functional_units.proto"], &["proto"])?;
+        .compile(&["../protos/functional_units.proto"], &["../protos"])?;
     Ok(())
 }
